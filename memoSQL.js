ff4673e377a18c1d3eab748d9eca80acdec6a521
Kyle Ledermann <kyle.ledermann@hotmail.com>
2013-11-25 11:25:14 -0500

    go

--- memoSQL.js
+++ memoSQL.js
@@ -1,66 +1,41 @@
-
-x$(document).on("deviceready", function () {
-});
-
-var listElement = x$('#listItems');
-var messageElement = x$('#message');
-var db;
-x$('#saveItem').on('click', function(e) {
-insertItem();
-});
-// Create a reference to the database
-function getDatabase() {
-return window.openDatabase("todoListDB",
-"1.0", "ToDoList Database", 200000);
-}
-
-// Run the onDeviceReady method
-onDeviceReady();
-// PhoneGap is ready
-function onDeviceReady() {
-db = getDatabase();
-db.transaction(function(tx) {
-tx.executeSql('CREATE TABLE IF NOT EXISTS MYLIST
-(id INTEGER PRIMARY KEY AUTOINCREMENT, list_action)');
-}, databaseError, getItems);
-}
-
-// Run a select statement to pull out all records
-function getItems() {
-db.transaction(function(tx) {
-tx.executeSql('SELECT * FROM MYLIST', [],
-querySuccess, databaseError);
-}, databaseError);
-}
-
-// Process the SQLResultSetList
-function querySuccess(tx, results) {
-var len = results.rows.length;
-var output = '';
-for (var i=0; i<len; i++){
-output = output +
-'<li id="' + results.rows.item(i).id + '">' +
-results.rows.item(i).list_action + '</li>';
-}
-messageElement.html('<p>There are ' + len +
-' items in your list:</p>');
-listElement.html('<ul>' + output + '</ul>');
-}
-
-// Insert a record into the database
-function insertItem() {
-var insertValue =
-document.getElementById('list_action').value;
-db.transaction(function(tx) {
-tx.executeSql('INSERT INTO MYLIST
-(list_action) VALUES ("' + insertValue + '")');
-}, databaseError, getItems);
-// Clear the value from the input box
-document.getElementById('list_action').value = '';
-}
-
-// Database error handler
-function databaseError(error) {
-messageElement.html("SQL Error: " + error.code);
-}
-
+//add listener when device ready
+    document.addEventListener("deviceready", onDeviceReady, false);
+    var db = window.openDatabase("Dummy_DB", "1.0", "Just a Dummy DB", 200000); //will create database Dummy_DB or open it
+ 
+    //function will be called when device ready
+    function onDeviceReady(){
+        db.transaction(populateDB, errorCB, successCB);
+    }
+ 
+    //create table and insert some record
+    function populateDB(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS SoccerPlayer (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Club TEXT NOT NULL)');
+        tx.executeSql('INSERT INTO SoccerPlayer(Name,Club) VALUES ("Alexandre Pato", "AC Milan")');
+        tx.executeSql('INSERT INTO SoccerPlayer(Name,Club) VALUES ("Van Persie", "Arsenal")');
+    }
+ 
+    //function will be called when an error occurred
+    function errorCB(err) {
+        alert("Error processing SQL: "+err.code);
+    }
+ 
+    //function will be called when process succeed
+    function successCB() {
+        alert("success!");
+        db.transaction(queryDB,errorCB);
+    }
+ 
+    //select all from SoccerPlayer
+    function queryDB(tx){
+        tx.executeSql('SELECT * FROM SoccerPlayer',[],querySuccess,errorCB);
+    }
+ 
+    function querySuccess(tx,result){
+        $('#SoccerPlayerList').empty();
+        $.each(result.rows,function(index){
+            var row = result.rows.item(index);
+            $('#SoccerPlayerList').append('<li><a href="#"><h3 class="ui-li-heading">'+row['Name']+'</h3><p class="ui-li-desc">Club '+row['Club']+'</p></a></li>');
+        });
+ 
+        $('#SoccerPlayerList').listview();
+    }
